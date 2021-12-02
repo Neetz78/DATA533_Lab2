88c06539bfd275aafc34f4a65c0f730b12817a76
Neetz78 <neethu.g0807@gmail.com>
2021-12-02 13:37:33 -0800

    class functions

---
+++ recipes/NonVeg/easy.py
@@ -0,0 +1,4 @@
+def ing(x):
+    print("Please select from the below ingredients")
+    v = input(f"{x.I[5],x.I[0],x.I[6],x.I[17]}: ")
+    return v
--- recipes/Veg/.ipynb_checkpoints/veg-checkpoint.py
+++ recipes/Veg/.ipynb_checkpoints/veg-checkpoint.py
@@ -1,4 +1,4 @@
 def level():
     print("Choose the difficulty level")
     level = input("Easy , Medium , Hard :")
-    return level
+    #if level=="S":
