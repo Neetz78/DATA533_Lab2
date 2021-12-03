24b558f9e687eb2d8b073ae5d9dddf0138fdf856
DishaDH123 <dishaholla04@gmail.com>
2021-12-02 16:24:58 -0800

    Vegetarian

--- recipes/Veg/.ipynb_checkpoints/easy-checkpoint.py
+++ recipes/Veg/.ipynb_checkpoints/easy-checkpoint.py
@@ -1,5 +1,38 @@
-def eing(x):
-    print("Please select from the below ingredients")
-    v = input(f"{x.I[1],x.I[2],x.I[3],x.I[4],x.I[7],x.I[8],x.I[9],x.I[10],x.I[11],x.I[12],x.I[13],x.I[14],x.I[15],x.I[16],x.I[18]}:")
+class easy:
+    v=[]
+    r=[]
+    rec=""
+    def __init__(self,x):
+        self.x=x
+    def select(self):
+        print("Please select from the below ingredients")
+        easy.v=[item for item in input(f"{self.x.I[1],self.x.I[2],self.x.I[3],self.x.I[4],self.x.I[7],self.x.I[8],self.x.I[9],self.x.I[10],self.x.I[11],self.x.I[12],self.x.I[13],self.x.I[14],self.x.I[15],self.x.I[16],self.x.I[18]}:").split()]
 
-    return v
+    
+    def search(self):
+        for items in easy.v:
+            for i in self.x.I:
+                if items==i:
+                    a=self.x[self.x['I'] == i].index.item()
+                    easy.r.append(self.x.R1[a])
+                    easy.r.append(self.x.R2[a])
+        print("Choose one dish from below choices")
+        easy.rec=input(f"{easy.r}:")
+        
+    def display(self):
+        for i in self.x.R1:
+                if easy.rec==i:
+                    ind=self.x[self.x['R1'] == i].index.item()
+                    print("Recipe blog link:",self.x.Y1[ind].split(" ")[0], "\n")
+                    print("Youtube video link:",self.x.Y1[ind].split(" ")[1], "\n")
+
+                    
+        for i in self.x.R2:
+                if easy.rec==i:
+                    ind=self.x[self.x['R2'] == i].index.item()
+                    print("Recipe blog link:",self.x.Y2[ind].split(" ")[0], "\n")
+                    print("Youtube video link:",self.x.Y2[ind].split(" ")[1], "\n")
+                    
+                   
+    
+    
--- recipes/Veg/intermediate.py
+++ recipes/Veg/intermediate.py
@@ -1,5 +1,38 @@
-def ming(x):
-    print("Please select from the below ingredients")
-    v = input(f"{x.I[1],x.I[2],x.I[3],x.I[4],x.I[7],x.I[8],x.I[9],x.I[10],x.I[11],x.I[12],x.I[13],x.I[14],x.I[15],x.I[16],x.I[18]}:")
+class medium:
+    v=[]
+    r=[]
+    rec=""
+    def __init__(self,x):
+        self.x=x
+    def select(self):
+        print("Please select from the below ingredients")
+        medium.v=[item for item in input(f"{self.x.I[1],self.x.I[2],self.x.I[3],self.x.I[4],self.x.I[7],self.x.I[8],self.x.I[9],self.x.I[10],self.x.I[11],self.x.I[12],self.x.I[13],self.x.I[14],self.x.I[15],self.x.I[16],self.x.I[18]}:").split()]
 
-    return v
+    
+    def search(self):
+        for items in medium.v:
+            for i in self.x.I:
+                if items==i:
+                    a=self.x[self.x['I'] == i].index.item()
+                    medium.r.append(self.x.R3[a])
+                    medium.r.append(self.x.R4[a])
+        print("Choose one dish from below choices")
+        medium.rec=input(f"{medium.r}:")
+        
+    def display(self):
+        for i in self.x.R3:
+                if medium.rec==i:
+                    ind=self.x[self.x['R3'] == i].index.item()
+                    print("Recipe blog link:",self.x.Y3[ind].split(" ")[0], "\n")
+                    print("Youtube video link:",self.x.Y3[ind].split(" ")[1], "\n")
+
+                    
+        for i in self.x.R4:
+                if medium.rec==i:
+                    ind=self.x[self.x['R4'] == i].index.item()
+                    print("Recipe blog link:",self.x.Y4[ind].split(" ")[0], "\n")
+                    print("Youtube video link:",self.x.Y4[ind].split(" ")[1], "\n")
+                    
+                   
+    
+    
